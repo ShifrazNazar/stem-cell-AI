bb0084127c948927e9dc85241747b5cb748205eb
Shifraz Nazar <109841959+ShifrazNazar@users.noreply.github.com>
2024-12-30 18:05:27 +0530

    fix cookie

--- server/src/app.ts
+++ server/src/app.ts
@@ -39,12 +39,15 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET!,
     resave: false,
-    saveUninitialized: false,
+    rolling: true,
+    saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
     cookie: {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      domain:
+        process.env.NODE_ENV === "production" ? ".vercel.app" : "localhost",
     },
   })
 );
