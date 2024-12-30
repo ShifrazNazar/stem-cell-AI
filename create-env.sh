71fc9ccff6471df570e651c60d05f412272296b6
Shifraz Nazar <109841959+ShifrazNazar@users.noreply.github.com>
2024-12-30 22:49:16 +0530

    fix bash script

--- create-env.sh
+++ create-env.sh
@@ -8,7 +8,10 @@ for dir in "${directories[@]}"; do
   if [ -f "$dir/.env.example" ]; then
     cp "$dir/.env.example" "$dir/.env"
     echo "Copied .env.example to .env in $dir"
+  elif [ -f "$dir/example.env" ]; then
+    cp "$dir/example.env" "$dir/.env"
+    echo "Copied example.env to .env in $dir"
   else
-    echo "No .env.example found in $dir"
+    echo "No .env.example or example.env found in $dir"
   fi
 done
