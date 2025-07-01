71491b45ad54e7d35816bce326cd59a3c52f27f8
Shifraz Nazar <109841959+ShifrazNazar@users.noreply.github.com>
2025-07-01 16:54:23 +0800

    session

--- server/src/app.ts
+++ server/src/app.ts
@@ -48,10 +48,9 @@ app.use(
     cookie: {
       httpOnly: process.env.NODE_ENV === "production",
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
-      domain:
-        process.env.NODE_ENV === "production" ? ".vercel.app" : ".vercel.app",
+      domain: process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
     },
   })
 );
