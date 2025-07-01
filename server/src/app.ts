37a63c1c89c93027ea26b6acdcdc8b22db1e0abd
Shifraz Nazar <109841959+ShifrazNazar@users.noreply.github.com>
2025-07-01 17:35:04 +0800

    fix samesite

--- server/src/app.ts
+++ server/src/app.ts
@@ -52,7 +52,7 @@ app.use(
     cookie: {
       httpOnly: process.env.NODE_ENV === "production" ? true : false,
       secure: process.env.NODE_ENV === "production" ? true : false,
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000,
       domain: process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
     },
